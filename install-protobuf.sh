7a59a858c248454244c058e6b0a09bccb63873d4
Andrew Brampton <bramp@google.com>
2016-02-14 11:45:10 -0800

    Changed the install-protobuf.sh not to download the protobuf library multiple times, and enabled ccache.

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
 # Small script to fetch the latest protobuf and installs it
-
 set -ex
-wget https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
-tar xvzf protobuf-cpp-3.0.0-beta-2.tar.gz
-cd protobuf-3.0.0-beta-2 && ./configure --prefix=$HOME && make &&  make install
 
-export LD_LIBRARY_PATH="$HOME/lib:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$HOME/lib:$LIBRARY_PATH"
+URL=https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protobuf-cpp-3.0.0-beta-2.tar.gz
+FILE=$(basename $URL)
+DIR=protobuf-3.0.0-beta-2
+DIST=$HOME/.dist
+DISTFILE=$DIST/$FILE
 
-export PATH="$HOME/bin:$PATH"
-export PKG_CONFIG_PATH="$HOME/lib/pkgconfig:$PKG_CONFIG_PATH"
-export C_INCLUDE_PATH="$HOME/include:$C_INCLUDE_PATH"
-export CPLUS_INCLUDE_PATH="$HOME/include:$CPLUS_INCLUDE_PATH"
+[ -d $DIST ] || mkdir $DIST
+
+if [[ -f $DISTFILE ]]; then
+  # not first run
+  curl -o $DISTFILE -z $DISTFILE -L $URL
+else
+  # first run
+  curl -o $DISTFILE -L $URL
+fi
+
+tar xvzf $DISTFILE
+cd $DIR && ./configure --prefix=$HOME && make &&  make install
